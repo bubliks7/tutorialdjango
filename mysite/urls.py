1c8f66861a95c27e3e65a2d7173f82ddd355e253
Bubliks <149240768+bubliks7@users.noreply.github.com>
2025-10-16 13:18:41 +0200

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,3 +21,8 @@ urlpatterns = [
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
 ]
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
